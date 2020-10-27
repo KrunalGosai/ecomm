c3c0ff2aa91415097106b5415d1f7e46a3f51629
TerenceWRoberts <Terence.roberts@inntechweb.com>
2020-10-27 13:14:28 +0530

    images updated

--- src/app/components/shop/home-three/home-three.component.ts
+++ src/app/components/shop/home-three/home-three.component.ts
@@ -24,11 +24,11 @@ export class HomeThreeComponent implements OnInit {
   public newArrivalsProducts: Array<Product>;
 
   public slides = [
-    { title: 'Huge sale', subtitle: 'Up to 70%', image: 'assets/images/carousel/banner1.jpg' },
-    { title: 'Biggest discount', subtitle: 'Check the promotion', image: 'assets/images/carousel/banner2.jpg' },
-    { title: 'Biggest sale', subtitle: 'Dont miss it', image: 'assets/images/carousel/banner3.jpg' },
-    { title: 'Our best products', subtitle: 'Special selection', image: 'assets/images/carousel/banner4.jpg' },
-    { title: 'Massive sale', subtitle: 'Only for today', image: 'assets/images/carousel/banner5.jpg' }
+    { title: 'Huge sale', subtitle: 'Up to 70%', image: 'https://img.freepik.com/free-vector/merry-christmas-banner-sale_91008-10.jpg' },
+    { title: '', subtitle: '', image: 'https://image.freepik.com/free-vector/big-sale-special-offer-banner-template_7547-308.jpg' },
+    { title: '', subtitle: '', image: 'https://cdni.iconscout.com/illustration/premium/thumb/halloween-offer-banner-997852.png' },
+    { title: '', subtitle: '', image: 'https://image.freepik.com/free-vector/special-offer-vibrant-banner-background_105164-558.jpg' }
+    // { title: 'Massive sale', subtitle: 'Only for today', image: 'assets/images/carousel/banner5.jpg' }
   ];
 
   constructor(private productService: ProductService, private cartService: CartService) { }
@@ -47,11 +47,11 @@ export class HomeThreeComponent implements OnInit {
 
      // Collection banner
      public discount = [{
-      image: 'assets/images/product/tablet_bn.png',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71hfs3%2BFRCL._AC_SL1500_.jpg',
       title: 'Tablets, Smartphones and more',
       subtitle: 'Sale up to 30%',
     }, {
-      image: 'assets/images/product/camera_bn.png',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/61ZL7iBKodL._AC_SL1000_.jpg',
       title: 'New Cameras Collection',
       subtitle: 'Sale up to 30%',
     }]
